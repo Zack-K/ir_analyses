6ddb0fe95793807aef836396961d928aab9e716a
Zack-K <gustokong.halohalo@gmail.com>
2025-06-23 11:51:50 +0900

    app.pyからAPI接続と分析をモジュール化して分離　動作確認はこれから行う

--- utils/api.py
+++ utils/api.py
@@ -6,6 +6,10 @@ import chardet
 import glob
 import pandas as pd
 from config.config import API_ENDPOINT, API_DOWNLOAD
+import logging
+
+# 標準ロガーの取得
+logger = logging.getLogger(__name__)
 
 """
 会社名を受取り、それに対応するdocuId(EDINETの書類ID)を返却する関数
@@ -29,9 +33,9 @@ def get_doc_id(sd_df, company_name:str):
 submiting_date: str fmt:yyyy-mm-dd
 API_KEY: str  EDINETのAPIキー(環境変数/config/シークレットから取得予定)
 
-return: pd.DataFrame or str
+return: pd.DataFrame or none
 """
-def get_company_list(submiting_date: str, API_KEY: str):
+def get_company_list(submiting_date: str, API_KEY: str) -> pd.DataFrame | None:
 
     try:
         # 「書類一覧API」のリクエストURLを作成
@@ -44,7 +48,7 @@ def get_company_list(submiting_date: str, API_KEY: str):
         response.raise_for_status()
         docs_submitted_json = response.json()
     except Exception as e:
-        return f"APIリクエストでエラー: {e}"
+        logger.error(f"APIに接続できませんでした。:{e}")
 
 
     # 取得データの確認
@@ -54,19 +58,20 @@ def get_company_list(submiting_date: str, API_KEY: str):
         sd_df = sd_df[sd_df["docDescription"].str.contains("四半期報告書", na=False)]
         return sd_df
     else:
-        err_message = "データが取得できませんでした。"
-        return err_message
+        logger.error("データが取得できませんでした。")
+        return None
 
 
 """
 財務データを企業ごとにフォルダーに分割、CSVデータをEDINET APIを通じてダウンロードする
 
+sd_df: pd.DataFrame 「書類一覧API」で取得したデータフレーム
+API_KEY: str         EDINET APIのAPIキー
 
+return company_financial_dataframe_dict
 """
-def get_financial_data(sd_df, API_KEY: str):
-
-    # TODO　現状ベタ打ちで2件のみ取得しているが、データフレームの行数分取得するようにする
-    company_name_list = sd_df["filerName"][:2]
+def fetch_financial_data(sd_df: pd.DataFrame, API_KEY: str) -> dict:
+    company_name_list = sd_df["filerName"]
 
     os.makedirs("download", exist_ok=True)
 
@@ -74,7 +79,7 @@ def get_financial_data(sd_df, API_KEY: str):
         doc_id = get_doc_id(sd_df, name)
         try:
             url = f"{API_DOWNLOAD}/documents/{doc_id}"
-            print(url)
+            logger.info(url)
             # EDINETの「書類取得API」に接続
             respose = requests.get(
                 url,
@@ -86,32 +91,33 @@ def get_financial_data(sd_df, API_KEY: str):
             )
 
             # csvファイルをzipで送られてくるので、会社毎にファイルを作成して配置
-            print(respose)
+            logger.info(respose)
             with zipfile.ZipFile(io.BytesIO(respose.content)) as z:
                 for file in z.namelist():
+                    # TODO 現在は四半期報告書のみに対応、将来的に有価証券報告書にも対応させたい
                     if file.startswith("XBRL_TO_CSV/jpcrp") and file.endswith(".csv"):
                         z.extract(file, path=f"download/{doc_id}")
-                        print(f"{name}:{doc_id}のファイルをダウンロードしました")
+                        logger.info(f"{name}:{doc_id}のファイルをダウンロードしました")
         except requests.exceptions.RequestException as e:
-            print(f"リクエスト中にエラーが発生しました: {e}")
+            logger.error(f"リクエスト中にエラーが発生しました: {e}")
         except zipfile.BadZipFile as e:
-            print(f"ZIPファイルの処理中にエラーが発生しました: {e}")
+            logger.error(f"ZIPファイルの処理中にエラーが発生しました: {e}")
 
-    dfs = {}
-
+    company_financial_dataframe_dict = {}
     for name in company_name_list:
         docId = get_doc_id(name)
         csvfile = glob.glob(f"download/{docId}/XBRL_TO_CSV/*.csv")
         if not csvfile:
-            print(f"CSVファイルが見つかりません: {docId}")
+            logger.error(f"CSVファイルが見つかりません: {docId}")
             continue
         csv_file_path = csvfile[0]
-        print(csv_file_path)
+        logger.info(csv_file_path)
         with open(csv_file_path, "rb") as f:
             raw_data = f.read()
             result = chardet.detect(raw_data)
             encoding = result["encoding"]
-            print(f"Detected encoding: {encoding}")
+            logger.info(f"Detected encoding: {encoding}")
 
-        dfs[name] = name
-        dfs[docId] = pd.read_csv(csv_file_path, encoding=encoding, delimiter="\t")
+        company_financial_dataframe_dict[name] = pd.read_csv(csv_file_path, encoding=encoding, delimiter="\t")
+
+    return company_financial_dataframe_dict
