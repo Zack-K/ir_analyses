974b7bd658c642379bce218b802ff0ffaaf36e64
Zack-K <gustokong.halohalo@gmail.com>
2025-08-04 11:45:28 +0900

    DBにUTF-8が対応できない設定になっていたバグを修正

--- utils/db_controller.py
+++ utils/db_controller.py
@@ -61,13 +61,13 @@ def get_db_engine():
         # 環境変数からDB接続情報を取得
         db_user = os.getenv("DB_USER", "user")
         db_password = os.getenv("DB_PASSWORD", "password")
-        db_host = os.getenv("DB_HOST", "db")  # Docker環境では"db"
+        db_host = os.getenv("DB_HOST", "db").strip()  # Docker環境では"db"
         db_port = os.getenv("DB_PORT", "5432")
         db_name = os.getenv("DB_NAME", "mydatabase")
 
         # TODO SQLAlchemyでPostgreSQLに接続するための設定を記載する
         connection_rul = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-        engine = create_engine(connection_rul, echo=True)
+        engine = create_engine(connection_rul, echo=True, connect_args={'options': '-c client_encoding=utf8'})
         logger.info("DBエンジン作成成功")
         return engine
 
