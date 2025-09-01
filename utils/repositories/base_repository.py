137aedf089f8f9b8e1a366e890aa5c509094990f
Zack-K <gustokong.halohalo@gmail.com>
2025-09-01 14:03:03 +0900

    Repositoryの責務についてコメントを追記

--- utils/repositories/base_repository.py
+++ utils/repositories/base_repository.py
@@ -1,9 +1,14 @@
 """
 Repositoryパターンの各クラスの基礎クラス
 
-責務
-(1)データベースセッションの保持
-(2)汎用CRUD操作
+* 責務（Do）
+1. データベースセッションの保持
+2. 汎用CRUD操作の提供
+3. 型安全性：具象Repositoryクラスの扱うモデルの型を明示する
+
+* 責務外（Don't）
+1. トランザクション管理：commit, rollback, closeは呼び出し元のService層の責務
+2. ドメイン固有のビジネスロジック提供：ジェネリクス（TypeVar）を使用して、具象Repositoryクラスのメソッドの引数の型を明示する
 """
 import os
 import logging
