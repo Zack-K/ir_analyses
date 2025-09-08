42c27737c7fe1ec569324915d07ed64ca50017cb
Zack-K <gustokong.halohalo@gmail.com>
2025-09-08 12:02:16 +0900

    create FinancialItemRepository 財務項目マスタテーブル用のリポジトリクラス　財務項目を管理する要素IDの有無をチェックするメソッドを持つ

---
+++ utils/repositories/financial_item_repository.py
@@ -0,0 +1,20 @@
+'''"""Financial_itemモデルのためのリポジトリクラス。
+
+    汎用的なCRUD操作はBaseRepositoryから継承し、
+    Financial_itemモデルに特化したデータアクセスロジックを提供します。
+    """
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from utils.db_models import Financial_item
+from utils.repositories.base_repository import BaseRepository
+
+class FinancialItemRepository(BaseRepository[Financial_item]):
+    def __init__(self, session: Session):
+        super().__init__(session, Financial_item)
+
+    def find_by_element_id(self, element_id:str) -> Financial_item | None:
+        statement = select(Financial_item).where(Financial_item.element_id == element_id)
+        result = self.session.scalars(statement).scalar_one_or_none()
+        return result
+'''
