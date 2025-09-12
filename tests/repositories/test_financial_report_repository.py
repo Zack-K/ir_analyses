2c77f21d7f8773b46d6bd21cdadc61be95b9accf
Zack-K <gustokong.halohalo@gmail.com>
2025-09-12 16:16:22 +0900

    update unit test正常系追加

--- tests/repositories/test_financial_report_repository.py
+++ tests/repositories/test_financial_report_repository.py
@@ -4,7 +4,8 @@ import pytest
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-from utils.repositories.financial_report_repository import Financial_report
+from utils.db_models  import Financial_report, Company
+from utils.repositories.financial_report_repository import FinancialReportRepository
 
 @pytest.fixture(scope="session")
 def engine():
@@ -33,7 +34,55 @@ def db_session(engine):
         db.close()
 
 @pytest.fixture(scope="function")
-def report_data()
-    financial_report = Financial_report(
-
+def company_data():
+    company = Company(
+        edinet_code="E12345",
+        security_code="S1234",
+        industry_code="TEST",
+        company_name="Test Company",
     )
+    return company
+
+@pytest.fixture(scope="function")
+def latest_report_data(company_data):
+    financial_report = Financial_report(
+        company = company_data,
+        document_type = "四半期報告書",
+        fiscal_year = 2024,
+        quarter_type = "Q1",
+        fiscal_year_end = "2024/3/30",
+        filing_date = "2024/1/10",
+    )
+    return financial_report
+
+
+@pytest.fixture(scope="function")
+def old_report_data(company_data):
+    financial_report = Financial_report(
+        company = company_data,
+        document_type = "四半期報告書",
+        fiscal_year = 2023,
+        quarter_type = "Q4",
+        fiscal_year_end = "2023/3/30",
+        filing_date = "2022/12/10",
+    )
+    return financial_report
+
+def test_find_latest_by_company_id(db_session, company_data, latest_report_data, old_report_data):
+    # Arrenge
+    repo = FinancialReportRepository(db_session)
+    repo.add(company_data)
+    repo.add(latest_report_data)
+    repo.add(old_report_data)
+    db_session.commit()
+    # Act
+
+    company_id = latest_report_data.company.company_id
+    result = repo.find_latest_by_company_id(company_id)
+    expected_result = latest_report_data
+    # Assert
+    assert result is not None
+    assert expected_result.fiscal_year == result.fiscal_year 
+    assert expected_result.report_id == result.report_id
+
+#TODO 異常系のテストを数種類
